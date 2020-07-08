c965518b8bc4db225cbb5b41a2fb91dbc3a4cd55
Alexis Leclercq <slakenote@gmail.com>
2020-07-08 17:51:10 +0200

    ajout de middleware de sécurité

--- P6_Leclercq_Alexis/api/app.js
+++ P6_Leclercq_Alexis/api/app.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const path = require('path')
 
 const sauceRoutes = require('./routes/sauce')
-const userRoutes = require('./routes/user')
+const userRoutes = require('./routes/user');
+const session = require('express-session');
 
 const app = express();
 
+// Connexion à la base de donnée mongoDB
 mongoose.connect('mongodb+srv://p6_oc_api:AcpHEYykiYvWa7J@cluster0-2ugsc.mongodb.net/pekocko?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -15,12 +17,25 @@ mongoose.connect('mongodb+srv://p6_oc_api:AcpHEYykiYvWa7J@cluster0-2ugsc.mongodb
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// Permet d'accéder à l'API depuis n'importe quelle origine
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+// Utilisation des cookies en http-only
+app.use(session({
+    secret: 'OpenClassrooms P6',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        domain: 'http://localhost:3000'
+    }
+}))
 
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
--- P6_Leclercq_Alexis/api/routes/user.js
+++ P6_Leclercq_Alexis/api/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-const userCtrl =  require('../controllers/user');
+const userCtrl = require('../controllers/user');
+const noCache = require('../middleware/noCache');
+const verifyPassword = require('../middleware/verifyPassword');
 
-router.post('/signup', userCtrl.signup)
-router.post('/login', userCtrl.login)
+router.post('/signup', noCache, verifyPassword, userCtrl.signup)
+router.post('/login', noCache, userCtrl.login)
 
 module.exports = router;
